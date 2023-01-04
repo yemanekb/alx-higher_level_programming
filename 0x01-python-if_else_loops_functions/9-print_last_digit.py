cf096d2d61c017888658fc7aca56fa5b9c260c5f
yemanekb <yemanekb@gmail.com>
2023-01-03 16:36:39 -0800

    11-pow.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-            number = number * -1
-        num = number % 10
-        print("{}".format(num), end='')
-        return num
+    if number > 0:
+        last_digit = number % 10
+    else:
+        last_digit = abs(number) % 10
+        print(last_digit, end="")
+        return last_digit
