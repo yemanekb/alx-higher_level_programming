9ce27425044d3437f2f1ab5dcb0eb71de2f860f0
yemanekb <yemanekb@gmail.com>
2023-01-08 04:33:44 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+
+
+def max_integer(my_list=[]):
+    """Returns largest int of a list"""
+    if my_list:
+        my_list.sort()
+        return my_list[-1]
+    else:
+        return None
