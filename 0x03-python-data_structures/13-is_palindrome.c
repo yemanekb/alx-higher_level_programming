0d6284c33aceb2ef622127849080bf98e2205732
yemanekb <113693054+yemanekb@users.noreply.github.com>
2023-01-08 16:21:45 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,139 +1,70 @@
 #include "lists.h"
-
 #include <stdio.h>
 
-
-
 int is_palindrome(listint_t **head)
-
 {
+  listint_t *nhead, *tort, *hare, *ptort;
+  listint_t *cut = NULL, *half, *it1, *it2;
 
-	  listint_t *nhead, *tort, *hare, *ptort;
+  if (!head || !*head)
+    return (1);
 
-	    listint_t *cut = NULL, *half, *it1, *it2;
+  nhead = *head;
+  if (nhead->next != NULL)
+    {
+      for (hare = nhead, tort = nhead; hare != NULL && hare->next != NULL;
+	   ptort = tort, tort = tort->next)
+	hare = hare->next->next;
+      if (hare != NULL)
+	{
+	  cut = tort;
+	  tort = tort->next;
+	}
+      ptort->next = NULL;
+      half = tort;
+      it1 = reverse_listint(&half);
+      for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
+	{
+	  if (it2->n != it1->n)
+	    return (0);
+	}
+      if (cut == NULL)
+	ptort->next = half;
+      else
+	{
+	  ptort->next = cut;
+	  cut->next = half;
+	}
+    }
 
-
-
-	      if (!head || !*head)
-
-		          return (1);
-
-
-
-	        nhead = *head;
-
-		  if (nhead->next != NULL)
-
-			      {
-
-				            for (hare = nhead, tort = nhead; hare != NULL && hare->next != NULL;
-
-							    	   ptort = tort, tort = tort->next)
-
-						    	hare = hare->next->next;
-
-					          if (hare != NULL)
-
-							  	{
-
-										  cut = tort;
-
-										  	  tort = tort->next;
-
-											  	}
-
-						        ptort->next = NULL;
-
-							      half = tort;
-
-							            it1 = reverse_listint(&half);
-
-								          for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
-
-										  	{
-
-													  if (it2->n != it1->n)
-
-														  	    return (0);
-
-													  	}
-
-									        if (cut == NULL)
-
-												ptort->next = half;
-
-										      else
-
-											      	{
-
-														  ptort->next = cut;
-
-														  	  cut->next = half;
-
-															  	}
-
-										          }
-
-
-
-		    return (1);
-
+  return (1);
 }
 
-
-
 /**
+ * reverse_listint - Reverses a linked list in pladce
+ * @head: Pointer to a pointer pointing to the first item in the list
  *
- *  * reverse_listint - Reverses a linked list in pladce
- *
- *   * @head: Pointer to a pointer pointing to the first item in the list
- *
- *    *
- *
- *     * Return: The new head of the reversed list
- *
- *      */
-
+ * Return: The new head of the reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
-
 {
+  listint_t *next = NULL, *prev = NULL;
 
-	  listint_t *next = NULL, *prev = NULL;
+  if (!head || !*head)
+    return (NULL);
 
+  while ((*head)->next)
+    {
+      next = (*head)->next;
 
+      (*head)->next = prev;
 
-	    if (!head || !*head)
+      prev = *head;
 
-		        return (NULL);
+      *head = next;
+    }
 
+  (*head)->next = prev;
 
-
-	      while ((*head)->next)
-
-		          {
-
-				        next = (*head)->next;
-
-
-
-					      (*head)->next = prev;
-
-
-
-					            prev = *head;
-
-
-
-						          *head = next;
-
-							      }
-
-
-
-	        (*head)->next = prev;
-
-
-
-		  return (*head);
-
+  return (*head);
 }
